8424f328bf0d75df0829e2afdc243ea4b9170607
Sina <dalvandsina@yahoo.com>
2020-08-28 02:30:29 +0430

    Application Created

---
+++ app/src/main/java/ir/sinadalvand/app/blurfragment/MainActivity.kt
@@ -0,0 +1,11 @@
+package ir.sinadalvand.app.blurfragment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
