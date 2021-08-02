6de88810f36366596f8f8de36a0cb804c3267123
Hamid Zandi <hzandi1989@gmail.com>
2021-08-03 00:00:06 +0430

    add sample TestDialogFragment

--- app/src/main/java/ir/sinadalvand/app/blurdialogfragment/MainActivity.kt
+++ app/src/main/java/ir/sinadalvand/app/blurdialogfragment/MainActivity.kt
@@ -2,10 +2,21 @@ package ir.sinadalvand.app.blurdialogfragment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val textView = findViewById<TextView>(R.id.helloTextView)
+        textView.setOnClickListener {
+            val blurDialogFragment = TestDialogFragment()
+            blurDialogFragment.show(
+                supportFragmentManager,
+                "test_fragment"
+            )
+        }
+
     }
 }
